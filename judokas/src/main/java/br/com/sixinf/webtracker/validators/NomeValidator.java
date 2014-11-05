dcbfdab301651c65f9d618e26da0696592dce900
Maicon Dante <maiconpas@yahoo.com.br>
2014-11-05 13:03:10 -0300

    Validação de nome abreviado e relatório de carteirinhas emitidas

---
+++ judokas/src/main/java/br/com/sixinf/webtracker/validators/NomeValidator.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package br.com.sixinf.webtracker.validators;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * @author maicon
+ *
+ */
+@FacesValidator(value="nomeValidator")
+public class NomeValidator implements Validator {
+
+	/**
+	 * 
+	 */
+	@Override
+	public void validate(FacesContext context, UIComponent component,
+			Object value) throws ValidatorException {
+		String nome = (String) value;
+		String[] partes = nome.split(" ");
+		for (String parte : partes) {
+			if (parte.length() == 1 ||
+					parte.contains(".") ||
+					parte.contains(","))
+				throw new ValidatorException(new FacesMessage("O Nome não pode ser abreviado"));
+		}
+	}
+
+}
